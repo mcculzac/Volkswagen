0364f04a32e1fb04a3ed8478778641cf6c35b53f
Zachary McCullough <mccul157@msu.edu>
2019-01-22 18:07:19 -0500

    update2

--- main_code/testing.py
+++
@@ -1,9 +0,0 @@
-import logging
-from tools.file_manipulation import open_file_on_desktop
-
-
-def __main__() -> None:
-    logging.basicConfig(filename='bot.log', level=logging.DEBUG)
-
-
-__main__()
